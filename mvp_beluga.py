567fc015597d6248e780a4e602a4b476fef26617
rafaelpcr <rafael@versex.com.br>
2025-03-05 15:06:38 -0300

    Update mvp_beluga.py

--- mvp_beluga.py
+++ mvp_beluga.py
@@ -314,7 +314,7 @@ def get_engagement():
         }), 500
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 3000))
+    port = int(os.getenv("PORT", 8080))
     host = os.getenv("HOST", "0.0.0.0")
     
     print("\n" + "="*50)
